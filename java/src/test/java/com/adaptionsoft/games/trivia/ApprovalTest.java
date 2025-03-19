3f307a1db58fc0870a42f9246689679ea7706c1f
Markus Harrer <github@markusharrer.de>
2025-03-19 08:47:53 +0100

    add internal diff tool usage

--- java/src/test/java/com/adaptionsoft/games/trivia/ApprovalTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/ApprovalTest.java
@@ -1,6 +1,9 @@
 package com.adaptionsoft.games.trivia;
+import com.adaptionsoft.games.trivia.runner.AiGameRunner;
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.approvaltests.Approvals;
+import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,6 +12,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 
+@UseReporter(JunitReporter.class)
 public class ApprovalTest {
 
     @Test
@@ -19,7 +23,7 @@ public class ApprovalTest {
         PrintStream ps = new PrintStream(baos);
         System.setOut(ps);
 
-        GameRunner.main(new String[0]);
+        AiGameRunner.main(new String[0]);
 
         System.out.flush();
         Approvals.verify(baos.toString());
