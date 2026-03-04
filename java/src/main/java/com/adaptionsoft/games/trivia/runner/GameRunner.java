b56c02a3bbbba5c4889a242ab4e0759011508de8
Markus <markus.harrer@innoq.com>
2026-03-04 16:34:48 +0100

    stabilized runner

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -16,8 +16,8 @@ public class GameRunner {
 		aGame.add("Pat");
 		aGame.add("Sue");
 		
-		Random rand = new Random();
-	
+		Random rand = new Random(42);
+
 		do {
 			
 			aGame.roll(rand.nextInt(5) + 1);
